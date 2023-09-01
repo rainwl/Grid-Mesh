90ac3eeb2b28efb6a9472021644199ad46a380d7
Rain <2215338912@qq.com>
2023-09-01 11:07:06 +0800

    Accelerated using Job System and Burst ; Increased by a factor of 427
    Accelerated using Job System and Burst, the speed increased by a factor of 427 compared to the original version. Generate time : 00:00:00.0439782

--- Assets/Scripts/MeshGridGenerator.cs
+++ Assets/Scripts/MeshGridGenerator.cs
@@ -1,6 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Burst;
 using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
@@ -15,14 +16,6 @@ public class MeshGridGenerator : MonoBehaviour
     {
         var stopwatch = new Stopwatch();
         stopwatch.Start();
-        GenerateGrid();
-        stopwatch.Stop();
-        var elapsedTime = stopwatch.Elapsed;
-        Debug.Log($"Generate time : {elapsedTime}");
-    }
-
-    private void GenerateGrid()
-    {
         var mesh = meshFilter.sharedMesh;
         var bounds = mesh.bounds;
 
@@ -33,14 +26,8 @@ public class MeshGridGenerator : MonoBehaviour
         );
 
         var gridStart = bounds.min + cellSize * 0.5f;
-        var collection = new GameObject("GridCollection")
-        {
-            transform =
-            {
-                position = bounds.center
-            }
-        };
-        var spheres = new List<GameObject>();
+        var cellCenters = new NativeArray<Vector3>(gridSize.x * gridSize.y * gridSize.z, Allocator.TempJob);
+
         for (var x = 0; x < gridSize.x; x++)
         {
             for (var y = 0; y < gridSize.y; y++)
@@ -48,68 +35,112 @@ public class MeshGridGenerator : MonoBehaviour
                 for (var z = 0; z < gridSize.z; z++)
                 {
                     var cellCenter = gridStart + new Vector3(x * cellSize.x, y * cellSize.y, z * cellSize.z);
-
-                    // ReSharper disable once InvertIf
-                    if (IsPointInsideMesh(cellCenter, mesh))
-                    {
-                        spheres.Add(CreateSphere(cellCenter, cellSize * 0.9f));
-                    }
+                    cellCenters[x + y * gridSize.x + z * gridSize.x * gridSize.y] = cellCenter;
                 }
             }
         }
 
-        foreach (var sphere in spheres)
+        var meshVertices = new NativeArray<Vector3>(mesh.vertices, Allocator.TempJob);
+        var triangles = new NativeArray<int>(mesh.triangles, Allocator.TempJob);
+        var results = new NativeArray<bool>(cellCenters.Length, Allocator.TempJob);
+
+        var generateGridJob = new GenerateGridJob
         {
-            sphere.transform.parent = collection.transform;
-        }
-        PrefabUtility.SaveAsPrefabAsset(collection, "Assets/Prefabs/" + prefabName + ".prefab");
-        Destroy(collection);
-    }
+            meshVertices = meshVertices,
+            triangles = triangles,
+            cellCenters = cellCenters,
+            results = results
+        };
 
-    private GameObject CreateSphere(Vector3 position, Vector3 scale)
-    {
-        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.position = position;
-        sphere.transform.localScale = scale;
-        Destroy(sphere.GetComponent<MeshRenderer>());
-        return sphere;
-    }
+        JobHandle jobHandle = generateGridJob.Schedule(cellCenters.Length, 32);
+        jobHandle.Complete();
 
-    private static bool IsPointInsideMesh(Vector3 point, Mesh mesh)
-    {
-        var triangles = mesh.triangles;
-        var ray = new Ray(point, Vector3.down);
-        var intersectCount = 0;
-        for (var i = 0; i < triangles.Length; i += 3)
+        var collection = new GameObject("GridCollection")
         {
-            var v1 = mesh.vertices[triangles[i]];
-            var v2 = mesh.vertices[triangles[i + 1]];
-            var v3 = mesh.vertices[triangles[i + 2]];
-
-            if (RayTriangleIntersection(ray, v1, v2, v3))
+            transform =
             {
-                intersectCount++;
+                position = bounds.center
+            }
+        };
+
+        for (int i = 0; i < cellCenters.Length; i++)
+        {
+            if (results[i])
+            {
+                CreateSphere(collection.transform, cellCenters[i], cellSize * 0.9f);
             }
         }
 
-        return intersectCount % 2 == 1;
+        PrefabUtility.SaveAsPrefabAsset(collection, "Assets/Prefabs/" + prefabName + ".prefab");
+        Destroy(collection);
+
+        cellCenters.Dispose();
+        meshVertices.Dispose();
+        triangles.Dispose();
+        results.Dispose();
+        stopwatch.Stop();
+        var elapsedTime = stopwatch.Elapsed;
+        Debug.Log($"Generate time : {elapsedTime}");
     }
 
-    private static bool RayTriangleIntersection(Ray ray, Vector3 v1, Vector3 v2, Vector3 v3)
+    [BurstCompile]
+    struct GenerateGridJob : IJobParallelFor
     {
-        var e1 = v2 - v1;
-        var e2 = v3 - v1;
-        var p = Vector3.Cross(ray.direction, e2);
-        var det = Vector3.Dot(e1, p);
-        if (det > -0.00001f && det < 0.00001f) return false;
-        var invDet = 1 / det;
-        var t = ray.origin - v1;
-        var u = Vector3.Dot(t, p) * invDet;
-        if (u < 0 || u > 1) return false;
-        var q = Vector3.Cross(t, e1);
-        var v = Vector3.Dot(ray.direction, q) * invDet;
-        if (v < 0 || u + v > 1) return false;
-        var tValue = Vector3.Dot(e2, q) * invDet;
-        return !(tValue < 0) && !(tValue > 1000);
+        [ReadOnly] public NativeArray<Vector3> meshVertices;
+        [ReadOnly] public NativeArray<int> triangles;
+        public NativeArray<Vector3> cellCenters;
+        public NativeArray<bool> results;
+
+        public void Execute(int index)
+        {
+            Vector3 cellCenter = cellCenters[index];
+            results[index] = IsPointInsideMesh(cellCenter);
+        }
+
+        private bool IsPointInsideMesh(Vector3 point)
+        {
+            var ray = new Ray(point, Vector3.down);
+            var intersectCount = 0;
+            for (var i = 0; i < triangles.Length; i += 3)
+            {
+                var v1 = meshVertices[triangles[i]];
+                var v2 = meshVertices[triangles[i + 1]];
+                var v3 = meshVertices[triangles[i + 2]];
+
+                if (RayTriangleIntersection(ray, v1, v2, v3))
+                {
+                    intersectCount++;
+                }
+            }
+
+            return intersectCount % 2 == 1;
+        }
+
+        private bool RayTriangleIntersection(Ray ray, Vector3 v1, Vector3 v2, Vector3 v3)
+        {
+            var e1 = v2 - v1;
+            var e2 = v3 - v1;
+            var p = Vector3.Cross(ray.direction, e2);
+            var det = Vector3.Dot(e1, p);
+            if (det > -0.00001f && det < 0.00001f) return false;
+            var invDet = 1 / det;
+            var t = ray.origin - v1;
+            var u = Vector3.Dot(t, p) * invDet;
+            if (u < 0 || u > 1) return false;
+            var q = Vector3.Cross(t, e1);
+            var v = Vector3.Dot(ray.direction, q) * invDet;
+            if (v < 0 || u + v > 1) return false;
+            var tValue = Vector3.Dot(e2, q) * invDet;
+            return !(tValue < 0) && !(tValue > 1000);
+        }
+    }
+
+    private void CreateSphere(Transform parent, Vector3 position, Vector3 scale)
+    {
+        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.SetParent(parent);
+        sphere.transform.position = position;
+        sphere.transform.localScale = scale;
+        Destroy(sphere.GetComponent<MeshRenderer>());
     }
 }
